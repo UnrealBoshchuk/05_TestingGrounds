01830dd7f4308a009898d8693478409e02b8c33d
Victor Boschuk <nakedsnake.boschuk@gmail.com>
2019-06-11 01:45:39 +0300

    323. Configuring the Gun

--- Source/S05_TestingGround/Player/FirstPersonCharacter.cpp
+++ Source/S05_TestingGround/Player/FirstPersonCharacter.cpp
@@ -54,12 +54,22 @@ void AFirstPersonCharacter::BeginPlay()
 	Super::BeginPlay();
 	if (GunBlueprint == NULL)
 	{
+		UE_LOG(LogTemp, Warning, TEXT("Gun blueprint missing."));
 		return;
 	}
+	
 	Gun = GetWorld()->SpawnActor<AGun>(GunBlueprint);
 
 	//Attach gun mesh component to Skeleton, doing it here because the skeleton is not yet created in the constructor
 	Gun->AttachToComponent(Mesh1P, FAttachmentTransformRules(EAttachmentRule::SnapToTarget, true), TEXT("GripPoint"));
+
+	Gun->AnimInstance = Mesh1P->GetAnimInstance();
+	if (EnableTouchscreenMovement(InputComponent) == false)
+	{
+		InputComponent->BindAction("Fire", IE_Pressed, Gun, &AGun::OnFire);
+	}
+	
+	
 }
 
 //////////////////////////////////////////////////////////////////////////
@@ -75,10 +85,10 @@ void AFirstPersonCharacter::SetupPlayerInputComponent(class UInputComponent* Pla
 	PlayerInputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
 
 	// Bind fire event
-//	PlayerInputComponent->BindAction("Fire", IE_Pressed, this, &AFirstPersonCharacter::OnFire);
+	//PlayerInputComponent->BindAction("Fire", IE_Pressed, Gun, &AGun::OnFire);
 
 	// Enable touchscreen input
-	EnableTouchscreenMovement(PlayerInputComponent);
+	//EnableTouchscreenMovement(PlayerInputComponent);
 
 	PlayerInputComponent->BindAction("ResetVR", IE_Pressed, this, &AFirstPersonCharacter::OnResetVR);
 
